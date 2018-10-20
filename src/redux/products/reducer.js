26a673a75ddf2685687d18d806be85e2f98e12f3
Patrick Shaughnessy <pshaughn86@gmail.com>
2018-10-20 09:11:17 -0700

    add redux

---
+++ src/redux/products/reducer.js
@@ -0,0 +1,11 @@
+export const PRODUCTS_INTIAL_STATE = {
+  productsList: [],
+  productsMap: {},
+};
+
+export const reducer = (state = PRODUCTS_INTIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
--- src/root.js
+++ src/root.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
+import { configureStore } from './redux';
 import App from './components/App';
 
+const store = configureStore();
+
 const mountNode = document.getElementById('root');
-ReactDOM.render(<App />, mountNode);
+
+const Application = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(Application, mountNode);
