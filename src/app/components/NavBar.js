7ff64be82669a41bc92ed15dd64479fa45c08618
Patrick Shaughnessy <pshaughn86@gmail.com>
2018-10-20 17:18:21 -0700

    update nav links

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">
-      Navbar
-    </a>
+    <Link to="/" className="navbar-brand">
+      A Nice Store
+    </Link>
     <button
       className="navbar-toggler"
       type="button"
@@ -18,61 +19,36 @@ const NavBar = () => (
     </button>
 
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item active">
-          <a className="nav-link" href="#">
-            Home
-            {' '}
-            <span className="sr-only">(current)</span>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-            Link
-          </a>
-        </li>
-        <li className="nav-item dropdown">
-          <a
-            className="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdown"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            Dropdown
-          </a>
-          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a className="dropdown-item" href="#">
-              Action
-            </a>
-            <a className="dropdown-item" href="#">
-              Another action
-            </a>
-            <div className="dropdown-divider" />
-            <a className="dropdown-item" href="#">
-              Something else here
-            </a>
-          </div>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="#">
-            Disabled
-          </a>
-        </li>
-      </ul>
-      <form className="form-inline my-2 my-lg-0">
-        <input
-          className="form-control mr-sm-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-          Search
-        </button>
-      </form>
+      <div className="navbar-nav mr-auto">
+        <Link to="/" className="nav-item nav-link">
+          Home
+        </Link>
+        <Link to="/sales" className="nav-item nav-link">
+          Sales
+        </Link>
+        <Link to="/about" className="nav-item nav-link">
+          About
+        </Link>
+        {/* <form className="nav-item form-inline">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-sm btn-outline-success" type="submit">
+            Search
+          </button>
+        </form> */}
+      </div>
+      <div className="nav justify-content-end">
+        <Link to="/cart" className="nav-item nav-link">
+          Cart
+        </Link>
+        <Link to="/account" className="nav-item nav-link">
+          Login
+        </Link>
+      </div>
     </div>
   </nav>
 );
