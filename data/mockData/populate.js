4dc5ec8298ff18773e7606c4461e7fd47f7c32db
Patrick Shaughnessy <pshaughn86@gmail.com>
2018-10-21 11:12:29 -0700

    update dummy data setup

--- data/mockData/populate.js
+++ data/mockData/populate.js
@@ -1,14 +1,29 @@
 const path = require('path');
 const fse = require('fs-extra');
+const mongoose = require('mongoose');
 const { Product } = require('../products');
 
 const populateProducts = async () => {
   try {
-    const filePath = path.resolve(__dirname, 'products.json');
-    const products = fse.readJson(filePath);
+    // TODO - extract to mongo utils
+    const dbUrl = 'mongodb://localhost/ecommerceApp';
+    const dbOptions = { useNewUrlParser: true };
+    mongoose.connect(
+      dbUrl,
+      dbOptions
+    );
+    const db = mongoose.connection;
+    db.on('error', e => console.error('error connecting to mongodb', e));
+    db.on('open', () => {
+      console.log('connected');
+    });
 
-    await Product.deleteMany();
+    const filePath = path.resolve(__dirname, 'products.json');
+    const products = await fse.readJson(filePath);
+    console.log('Prepared products:', products.length);
+    await Product.remove({});
     await Promise.all(products.map(product => Product.create(product)));
+    console.log('Populated new products into DB');
   } catch (e) {
     console.error(`Error populating products to mongo: ${e.message}`);
   }
