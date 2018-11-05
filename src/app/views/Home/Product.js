39463123fb1ed65187e6b995788dc3ce4cd53b2b
Patrick Shaughnessy <pshaughn86@gmail.com>
2018-11-04 17:42:59 -0800

    update home background css

--- src/app/views/Home/Product.js
+++ src/app/views/Home/Product.js
@@ -6,10 +6,13 @@ import { formatPrice } from '../../../../lib/utils';
 const Product = ({ _id, name, images, shortDescription, price }) => (
   <Link to={`/products/${_id}`} className="product">
     <div className="card">
-      <div className="card-header">
-        <img src={images[0].src} alt={images[0].alt} />
-      </div>
       <div className="card-body">
+        <img
+          className="shadow rounded img-thumbnail"
+          src={images[0].src}
+          alt={images[0].alt}
+        />
+        <hr />
         <h5 className="card-title">{name}</h5>
         <h6 className="card-subtitle mb-2 text-muted">
           {`$${formatPrice(price)}`}
